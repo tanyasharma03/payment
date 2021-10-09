624319ee5b1bca244374945d3aab92c62dee9289
Tanya Sharma <tasu2612@gmail.com>
2021-10-09 23:14:39 +0530

    form completed

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,6 +20,44 @@ const Home = () => {
         setUser({...user, [name] :value });
     }
 
+    const postData = async(event)=>{
+
+        event.preventDefault();
+
+        const {fname,email,address,state,country} = user;
+
+        if( !fname || !email || !address || !state || !country){
+            alert("Fill details properly")
+        }else{
+            const res = await fetch(
+                "https://payment-e0619-default-rtdb.firebaseio.com/paymentForm.json",
+                {
+                method:"POST",
+                headers:{
+                    "Content-Type":"application/json",
+                },
+                body:JSON.stringify({
+                    fname,
+                    email,
+                    address,
+                    state,
+                    country
+                }),
+            }
+            );
+            if(res){
+                setUser({
+                    fname:"",
+                    email:"",
+                    address:"",
+                    state:"",
+                    country:""
+                });
+                alert("Data stored successfully");
+            }
+        }
+        } ;
+
     return (
         <>
             <div className="container">
@@ -32,32 +70,32 @@ const Home = () => {
                          <div className="col-lg-6 order-lg-2 order-2">
                              <h2>Payment Information</h2>
 
-                             
+                             <form method="POST">
                              <div className="details">
-
+                         
                              <i class="zmdi zmdi-account icons"></i>
-                             <TextField id="standard-basic" className="input-box" type="text" value={user.fname} onChange={getUserData} name="fname" label="Full Name" variant="standard" />
+                             <TextField id="standard-basic" className="input-box" type="text" value={user.fname} onChange={getUserData} name="fname" label="Full Name" variant="standard" autoComplete="off" required />
                              <br/>
 
                              <i class="zmdi zmdi-email icons"></i>
-                             <TextField id="standard-basic" className="input-box" value={user.email} onChange={getUserData} type="email" name="email"  label="Email" variant="standard" />
+                             <TextField id="standard-basic" className="input-box" value={user.email} onChange={getUserData} type="email" name="email"  label="Email" variant="standard" autoComplete="off" required />
                              <br/>
 
                              <i class="zmdi zmdi-home icons"></i>
-                             <TextField id="standard-basic" className="input-box" value={user.address} onChange={getUserData} type="text"  name="address" label="Address" variant="standard" />
+                             <TextField id="standard-basic" className="input-box" value={user.address} onChange={getUserData} type="text"  name="address" label="Address" variant="standard" autoComplete="off" required/>
                              <br/>
 
                              <i class="zmdi zmdi-pin icons"></i>
-                             <TextField id="standard-basic" className="input-box"  value={user.state} onChange={getUserData} type="text"  name="state" label="State" variant="standard" />
+                             <TextField id="standard-basic" className="input-box"  value={user.state} onChange={getUserData} type="text"  name="state" label="State" variant="standard" autoComplete="off" required/>
                              <br/>
 
                              <i class="zmdi zmdi-flag icons"></i>
-                             <TextField id="standard-basic" className="input-box" value={user.country} onChange={getUserData} type="text"  name="country" label="Country" variant="standard" />
+                             <TextField id="standard-basic" className="input-box" value={user.country} onChange={getUserData} type="text"  name="country" label="Country" variant="standard" autoComplete="off" required/>
                              <br/>
 
-                             <button className="btn btn-primary btn-pay">Pay Now</button>
+                             <button className="btn btn-primary btn-pay" onClick={postData}>Pay Now</button>
                              </div>
-                             
+                             </form>
                          </div>
                      </div>
                  </div>
